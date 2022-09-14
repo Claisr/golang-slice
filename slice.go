73ab03184a1cdd857eb055045983af3181dea300
Eggy fhr <22157337+iameggi@users.noreply.github.com>
2022-09-15 01:27:38 +0700

    fix variable
    there is wrong, u not add variable in "make" and dont put semicolns

--- slice.go
+++ slice.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	a := make([]int, 2, s)
-	a[0] = 10;
-	a[1] = 20;
+	a := make([]int, 2, 5)
+	a[0] = 10
+	a[1] = 20
 	fmt.Println("Slice A:", a)
 	fmt.Printf("Length is %d Capacity is %d\n", len(a), cap(a))
 
